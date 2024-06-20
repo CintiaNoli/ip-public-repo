c7a11bab40996de1065ff6997f115d2a2a0fc8c8
CintiaNoli <169107926+CintiaNoli@users.noreply.github.com>
2024-06-20 16:06:34 -0300

    Actualizar urlsviewactualizado.py

--- nasa_image_gallery/urlsviewactualizado.py
+++ nasa_image_gallery/urlsviewactualizado.py
@@ -3,14 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='index-page'),
-    path('login/', views.index_page, name='login'),
-    path('home/', views.home, name='home'),
-    path('buscar/', views.search, name='buscar'),
-
-    path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
-    path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
-    path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
-
-    path('exit/', views.exit, name='exit'),
+    # Páginas principales
+    path('', views.index_page, name='index-page'),  # Página principal
+    path('login/', views.index_page, name='login'),  # Página de inicio de sesión
+    
+    # Páginas de la aplicación
+    path('home/', views.home, name='home'),  # Página de inicio (dashboard)
+    path('buscar/', views.search, name='buscar'),  # Página de búsqueda
+    
+    # Favoritos
+    path('favourites/', views.getAllFavouritesByUser, name='favoritos'),  # Lista de favoritos del usuario
+    path('favourites/add/', views.saveFavourite, name='agregar-favorito'),  # Agregar un favorito
+    path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),  # Borrar un favorito
+    
+    # Salida (cerrar sesión)
+    path('exit/', views.exit, name='exit'),  # Página de salida (cerrar sesión)
 ]
